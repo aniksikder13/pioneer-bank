d30d29f42257374fef811b1bda0269e7e6c4f245
Anik Sikder <aniksikder9501@gmail.com>
2023-02-14 17:57:05 +0600

    Final project

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const login= document.getElementById('login')
-const loingArea= document.getElementById('loginArea')
-const transactionArea= document.getElementById('transactionArea')
+function getElement(id) {
+    return document.getElementById(id)
+}
 
-login.addEventListener('submit', function(e){
+getElement('login').addEventListener('submit', function(e){
     e.preventDefault()
-    loingArea.style.display='none'
-    transactionArea.style.display='flex'
+    getElement('loginArea').style.display='none'
+    getElement('transactionArea').style.display='flex'
+})
+
+function setAmount(id1, id2){
+    const value= parseFloat(getElement(id1).value)
+    const currentValue= parseFloat(getElement(id2).innerText)
+    return value+currentValue  
+}
+
+function setAmount2(id1, id2){
+    const value= parseFloat(getElement(id1).value)
+    const currentValue= parseFloat(getElement(id2).innerText)
+    return currentValue-value
+}
+
+
+getElement('depositSubmit').addEventListener('submit', function(e){
+    e.preventDefault()
+    getElement('deposit').innerText= setAmount('depositInput', 'deposit')
+    getElement('balance').innerText= setAmount('depositInput', 'balance')
+})
+
+getElement('withdrawSubmit').addEventListener('submit', function(e){
+    e.preventDefault()
+    getElement('withdraw').innerText= setAmount('withdrawInput', 'withdraw')
+    getElement('balance').innerText= setAmount2('withdrawInput', 'balance')
 })
